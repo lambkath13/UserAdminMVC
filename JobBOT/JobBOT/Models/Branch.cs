7aa539dee0edc7127402b0f8ed97eef7a75dc188
lambkath13 <avazuldasev70@gmail.com>
2025-10-04 00:09:33 +0500

    Delete JobBOT/JobBOT directory

--- JobBOT/JobBOT/Models/Branch.cs
+++
@@ -1,9 +0,0 @@
-namespace JobBOT.Models;
-
-public class Branch
-{
-    public Guid Id { get; set; }
-    public required string Name { get; set; } 
-    
-    List<Vacancy>? Vacancies { get; set; } 
-}
--- JobBOT/JobBOT/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace JobBOT.Models;
-
-public class User
-{
-    public Guid Id { get; set; }
-    public required string Name { get; set; }
-    public bool IsAdmin { get; set; }
-    public bool IsSuperAdmin { get; set; }
-}
